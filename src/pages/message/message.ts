65beb3fa2967eab7cadde0dbcf049fddf2d48961
CK <chenkai4dev@163.com>
2017-07-06 15:19:41 +0800

    工作区Tab页样式确定

--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -7,11 +7,32 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MessagePage {
 
+  searchQuery: string = '';
+  items: string[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MessagePage');
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
