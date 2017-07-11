749949b68ca8a89b1b475fda56e3d0d4683aff26
CK <chenkai4dev@163.com>
2017-07-11 17:26:56 +0800

    增加 jpush

---
+++ src/providers/native-service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import {Platform} from "ionic-angular";
+
+@Injectable()
+export class NativeService{
+
+  constructor(private platform: Platform) {
+
+  }
+
+  /**
+   * 是否真机环境
+   */
+  isMobile(): boolean {
+    return this.platform.is('mobile') && !this.platform.is('mobileweb');
+  }
+
+  /**
+   * 是否android真机环境
+   */
+  isAndroid(): boolean {
+    return this.isMobile() && this.platform.is('android');
+  }
+
+  /**
+   * 是否ios真机环境
+   */
+  isIos(): boolean {
+    return this.isMobile() && (this.platform.is('ios') || this.platform.is('ipad') || this.platform.is('iphone'));
+  }
+
+}
