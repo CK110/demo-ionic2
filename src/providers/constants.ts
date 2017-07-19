1ff72631dec6b3bb7acc2bda6babaf172c85c0e6
CK <chenkai4dev@163.com>
2017-07-19 16:20:53 +0800

    非热更新方式

---
+++ src/providers/constants.ts
@@ -0,0 +1,17 @@
+
+
+export const APP_DOWNLOAD = 'http://omzo595hi.bkt.clouddn.com/download.html';//app下载地址
+export const APK_DOWNLOAD = 'http://omzo595hi.bkt.clouddn.com/ionic2_tabs.apk';//apk下载完整地址
+
+/**
+ * xml格式
+ * <update>
+ *  <version>302048</version>
+ *  <name>APK Name</name>
+ *  <url>https://your-remote-api.com/YourApp.apk</url>
+ * </update>
+ * @type {string}
+ */
+export const APK_VERSION_UPDATE ='http://your-remote-api.com/update.xml'
+
+
--- src/providers/native-service.ts
+++ src/providers/native-service.ts
@@ -1,13 +1,27 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
-import {Platform} from "ionic-angular";
+import {AlertController, Platform} from "ionic-angular";
 import {Network} from "@ionic-native/network";
+import {InAppBrowser} from "@ionic-native/in-app-browser";
+import {Observable} from "rxjs";
+import {AppVersion} from "@ionic-native/app-version";
+import {APK_DOWNLOAD, APK_VERSION_UPDATE, APP_DOWNLOAD} from "./constants";
+import { FileTransfer, FileTransferObject} from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { AppUpdate } from '@ionic-native/app-update';
+
 
 @Injectable()
 export class NativeService{
 
   constructor(private platform: Platform,
-              private network: Network) {
+              private network: Network,
+              public alertCtrl: AlertController,
+              private inAppBrowser: InAppBrowser,
+              public appVersion:AppVersion,
+              public file: File,
+              private transfer: FileTransfer,
+              public appUpdate: AppUpdate) {
 
   }
 
@@ -48,6 +62,90 @@ export class NativeService{
   isConnecting(): boolean {
     return this.getNetworkType() != 'none';
   }
+
+  /**
+   * 检查app是否需要升级
+   */
+  detectionUpgrade(): void {
+    //这里连接后台判断是否需要升级,不需要升级就return
+
+    this.alertCtrl.create({
+      title: '升级',
+      subTitle: '发现新版本,是否立即升级？',
+      buttons: [{text: '取消'},
+        {
+          text: '确定',
+          handler: () => {
+            this.downloadApp();
+          }
+        }
+      ]
+    }).present();
+
+  }
+
+  /**
+   * 下载安装app
+   */
+  downloadApp(){
+    if(this.isAndroid()){
+
+      this.appUpdate.checkAppUpdate(APK_VERSION_UPDATE);
+
+      // this.apkDowloadPress();
+
+
+    }else {
+      this.inAppBrowser.create(APP_DOWNLOAD,'_system')
+    }
+  }
+
+  /**
+   * 另一种apk更新方式
+   * 1.File http://ionicframework.com/docs/native/file/
+   * 2.Transfer http://ionicframework.com/docs/native/file-transfer/
+   * 3.install https://github.com/weelion/phonegap-plugin-install
+   */
+  apkDowloadPress(){
+    let alert = this.alertCtrl.create({
+      title: '下载进度：0%',
+      enableBackdropDismiss: false,
+      buttons: ['后台下载']
+    });
+    alert.present();
+
+    const fileTransfer: FileTransferObject = this.transfer.create();
+    const apk = this.file.externalRootDirectory + 'android.apk'; //apk保存的目录
+
+    fileTransfer.download(APK_DOWNLOAD, apk).then(() => {
+      window['install'].install(apk.replace('file://', ''));
+    });
+
+    fileTransfer.onProgress((event: ProgressEvent) => {
+      let num = Math.floor(event.loaded / event.total * 100);
+      if (num === 100) {
+        alert.dismiss();
+      } else {
+        let title = document.getElementsByClassName('alert-title')[0];
+        title && (title.innerHTML = '下载进度：' + num + '%');
+      }
+    });
+  }
+
+  /**
+   * 获得app版本号,如0.0.1
+   * @description  对应/config.xml中version的值
+   */
+  getVersionNumber(): Observable<string> {
+    return Observable.create(observer => {
+      this.appVersion.getVersionNumber().then((value: string) => {
+        observer.next(value);
+      }).catch(err => {
+        console.log('%cNativeService/' + 'getVersionNumber:' + err, 'color:#C41A16');
+      });
+    });
+  }
+
 
 
 
